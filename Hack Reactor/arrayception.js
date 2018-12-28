72717c9b2cf115501a5bb8018a7c868870f877eb
Dominique Robinson <domrobinson.dj@gmail.com>
2018-12-27 23:53:31 -0600

    complete-arrayception

---
+++ Hack Reactor/arrayception.js
@@ -0,0 +1,22 @@
+/*
+Arrayception
+Given an array of arbitrarily nested arrays, return n, where n is the deepest level that contains a non-array value.
+*/
+
+const arrayception = array => {
+  var max = 0;
+
+  for (var i = 0; i < array.length; i++) {
+    if (array[i].length === 0) {
+      count = 0;
+    } else if (Array.isArray(array[i])) {
+      count = arrayception(array[i]);
+      if (count > 0) count += 1;
+    } else {
+      count = 1;
+    }
+
+    if (count > max) max = count;
+  }
+  return max;
+};
