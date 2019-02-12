450ad37c1e2dcc1bdbdab7aecad30811092f8da5
Dominique Robinson <33168202+domjrobinson@users.noreply.github.com>
2019-02-11 23:31:51 -0600

    Update Finding-all-matching-strings-in-an-array.js

--- Hack Reactor/Finding-all-matching-strings-in-an-array.js
+++ Hack Reactor/Finding-all-matching-strings-in-an-array.js
@@ -4,7 +4,7 @@ Given a length (number) and an array of strings, return all strings that are gre
 
 DO NOT use .filter() in your solution.
 
-DO use a for loop.
+DO use a for loop
 
 TRY solving it with non-mutative recursion, only after you’ve solved it using a for loop.
 */
